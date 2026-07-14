7db63c3fc9b0ffae1a45eba27f3f8b4e0d278b54
Daniela Verçosa <daniela.jhenni@hotmail.com>
2026-07-14 13:53:44 -0300

    Add apresentacao.py with personal introduction

---
+++ aula-02/apresentacao.py
@@ -0,0 +1,9 @@
+nome = "Daniela"
+idade = 25
+cidade = "Garanhuns"
+objetivo = "Migrar para a área de Tecnologia"
+
+print("Olá, meu nome é", nome)
+print("Tenho", idade, "anos")
+print("Moro em", cidade)
+print("Meu objetivo é", objetivo)
